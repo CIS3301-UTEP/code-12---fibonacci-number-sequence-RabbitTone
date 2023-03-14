b11f2a8195f26746c857504d989da238816fd24d
RabbitTone <48339091+RabbitTone@users.noreply.github.com>
2023-03-14 00:00:09 -0600

    Followed instructions | Attempt #1

--- code_12.py
+++ code_12.py
@@ -1,8 +1,10 @@
 def get_fibonacci_number(position):
-    pass #Remove this line and insert your code here. Do not forget this function implements recursion.
+    if position <= 2:
+        return 1
+    return get_fibonacci_number(position-1) + get_fibonacci_number(position-2)
 
 def get_fibonacci_number_sequence(number):
-    pass #Remove this line and insert your code here. Do not forget to use get_fibonacci_number to create your list of numbers.
+    return [get_fibonacci_number(i) for i in range(1, number+1)]
 
 if __name__ == "__main__":
-    pass #Remove this line and insert your code to test your Fibonacci function here
+    pass
